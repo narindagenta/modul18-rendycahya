c8897e06bfcde7e4df50cfec35de6c2f021dae12
narindagenta <ngentasiacc@gmail.com>
2022-12-04 19:27:25 +0700

    Initial commit

---
+++ src/pages/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PageNotFound from '../pages/404.png';
+
+// Modifikasi halaman berikut untuk menjadi halaman 404 yang menarik!
+const NotFound = () => {
+  return <div>
+    <h1>Halaman tidak ditemukan</h1>
+     </div>
+};
+
+export default NotFound
